299f1afd2021e7cae434da8871c08114565068bc
Jonajubert <124742963+Jonajubert@users.noreply.github.com>
2026-07-19 11:57:26 -0300

    Add variable declaration and output to console

--- 001-Variables/variables.cs
+++ 001-Variables/variables.cs
@@ -1,1 +1,19 @@
+using System;
 
+namespace CSharpDesdeCero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de variables
+            string nombre = "Jonatan";
+            int edad = 30;
+
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Edad: " + edad);
+
+            Console.ReadKey();
+        }
+    }
+}
